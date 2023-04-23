ad9724bc770ba849bbf399a0b6bb4669dd5fd19e
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-04-23 13:22:37 +0800

    feat: add tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const path = require("path");
 const Config = require("webpack-chain");
 
 const config = new Config();
+
+// add entry
+config.entry("app").add(path.resolve(__dirname, "src/app/index.ts"));
+
 module.exports = () => config.toConfig();
