a2d0a2bce9ef69d82c75c1942c6230453d3d48b1
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-04-23 20:55:47 +0800

    feat: add terser plugin

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import typescript from "rollup-plugin-typescript2";
+import terser from "@rollup/plugin-terser";
 
 export default {
     input: {
@@ -8,5 +9,10 @@ export default {
         dir: "./generators",
         format: "cjs",
     },
-    plugins: [typescript()],
+    plugins: [
+        typescript(),
+        terser({
+            maxWorkers: 2,
+        }),
+    ],
 };
