1f7bab6e3548868fb0a6a3564c141397002285d9
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-04-23 12:35:09 +0800

    feat: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["@typescript-eslint", "prettier"],
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+        "prettier/prettier": [
+            "error",
+            {},
+            {
+                usePrettierrc: true,
+            },
+        ],
+    },
+};
