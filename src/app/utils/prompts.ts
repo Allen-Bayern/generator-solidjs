64bdd6120d49766730b0e0571730a5df197449cf
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-04-24 13:26:18 +0800

    feat: add a question

--- src/app/utils/prompts.ts
+++ src/app/utils/prompts.ts
@@ -1,9 +1,21 @@
 import type { Question } from "yeoman-generator";
 
-export const questions: Question[] = [
+// Prefixes
+const language = "[language]";
+const feature = "[feature]";
+
+interface UsedAnswers {
+    // 语言
+    language: string;
+    // 是否用TS
+    isTsNeeded: boolean;
+}
+
+export const questions: Question<UsedAnswers>[] = [
     // 问题一: 使用中文还是英文
     {
         type: "list",
+        name: "language",
         choices: [
             {
                 name: "English",
@@ -11,11 +23,28 @@ export const questions: Question[] = [
             },
             {
                 name: "中文",
-                value: "cn",
+                value: "zhCn",
             }
         ],
         default: "en",
-        prefix: "[language]",
-        message: "Choose your language: "
+        prefix: language,
+        message: "Choose your language:"
     },
+    // 问题二: 是否需要ts
+    {
+        type: "confirm",
+        name: "isTsNeeded",
+        default: true,
+        prefix: feature,
+        message({ language }) {
+            switch(language) {
+                case "zhCn": {
+                    return "是否使用TypeScript? ";
+                }
+                default: {
+                    return "Use TypeScript? ";
+                }
+            }
+        }
+    }
 ];
