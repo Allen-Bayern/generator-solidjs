a8a62b6a2cca016d444d88402622f378bb44890f
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-05-02 00:48:42 +0800

    fix: config

--- src/app/templates/webpack.config.js
+++ src/app/templates/webpack.config.js
@@ -5,10 +5,14 @@ const config = useBasicConfig(process.env.NODE_ENV ?? "development");
 /**
  * If you want to add or modify the webpack config,
  * you can use webpack-chain like the function below.
+ * open all the // below, including function and chainWebpack().
  * The documentation: https://github.com/neutrinojs/webpack-chain
  */
+
 // function chainWebpack() {
 //     config.
 // }
+
+// chainWebpack();
 
 module.exports = config.toConfig();
